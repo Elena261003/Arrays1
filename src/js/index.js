c91f108b7173ba3290f6225875de8d6bedfc39a0
UMessina <samsonova.lena.03@gmail.com>
2024-05-20 18:50:28 +0200

    ggg

--- src/js/index.js
+++ src/js/index.js
@@ -16,3 +16,30 @@ const getNumber = number => {
   console.log(media);
 };
 getNumber([1, 2, 3]);
+
+const getAleatoryNumber = number => {
+  const getNumberRandom = Math.floor(Math.random() * 11);
+  if (number.includes(getNumberRandom)) {
+    console.log(number(3));
+  }
+};
+
+const nombreDeLaFuncion = (datosDeLaFuncion, otroDato) => {
+  console.log(datosDeLaFuncion + otroDato);
+};
+nombreDeLaFuncion(10, 7);
+
+const randomFunction = (a, b) => {
+  a.push(b);
+  console.log(a);
+};
+randomFunction([1, 2, 3], 4);
+
+const a = 2;
+const b = 'palabra';
+const c = []; // array
+const d = () => {
+  // Function
+};
+
+const nameOfFunction = dato => {};
